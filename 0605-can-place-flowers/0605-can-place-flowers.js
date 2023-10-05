0be612fdad7c3a2d750eb9aebe7a9df7cbc0f46b
Taesung Yun <cheonyulin@gmail.com>
2023-10-05 09:10:15 +0900

    Time: 55 ms (70.38%), Space: 44.2 MB (60.93%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,37 +4,17 @@
  * @return {boolean}
  */
 const canPlaceFlowers = function(flowerbed, n) {
-    if(flowerbed.length === 1 && flowerbed[0] === 0) {
-        return true;
-    }
-
-    const lastIndex = flowerbed.length - 1;
     let count = 0;
 
-    for(let i=0; i<flowerbed.length; i++) {
-        if(i === 0) {
-            if(flowerbed[0] === 0 && flowerbed[1] === 0) {
-                flowerbed[0] = 1;
-                count += 1;
-            }
-        }
+    flowerbed.forEach((_, index) => {
+        const prev = index - 1;
+        const next = index + 1;
 
-        if(i === lastIndex) {
-            if(flowerbed[i-1] === 0 && flowerbed[i] === 0) {
-                flowerbed[i] = 1;
-                count += 1;
-            }
-        }
-
-        if(flowerbed[i-1] === 0 && flowerbed[i] === 0 && flowerbed[i+1] === 0) {
-            flowerbed[i] = 1;
+        if(!flowerbed[prev] && !flowerbed[index] && !flowerbed[next]) {
             count += 1;
+            flowerbed[index] = 1;
         }
+    });
 
-        if(count >= n) {
-            return true;
-        }
-    }
-
-    return false;
+    return count >= n;
 };
