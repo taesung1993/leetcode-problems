17ca75b7922d5688ef2b596d7d424fa97961c99e
Taesung Yun <cheonyulin@gmail.com>
2023-10-24 10:46:10 +0900

    Time: 64 ms (97%), Space: 45.6 MB (51.96%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -1,24 +1,24 @@
-const isVowel = (char) => {
-    switch(char) {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            return true;
-        default:
-            return false;
-    }
-}
-
 /**
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
 const maxVowels = function(s, k) {
-    let max = 0;
+    const isVowel = (char) => {
+        switch(char) {
+            case 'a':
+            case 'e':
+            case 'o':
+            case 'i':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+
     let count = 0;
+    let max = 0;
 
     for(let i=0; i<s.length; i++) {
         if(i >= k) {
@@ -26,8 +26,8 @@ const maxVowels = function(s, k) {
         }
 
         count += isVowel(s[i]);
-        max = Math.max(max, count);
+        max = Math.max(count, max);
     }
 
-    return max;   
+    return max;
 };
