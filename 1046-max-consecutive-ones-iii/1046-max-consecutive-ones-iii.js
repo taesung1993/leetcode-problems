e6f5919d723b1622f3b1c682af2ce90234c88348
Taesung Yun <cheonyulin@gmail.com>
2023-10-24 11:09:48 +0900

    Time: 68 ms (37.37%), Space: 46.8 MB (59.69%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
@@ -9,20 +9,20 @@ const longestOnes = function(nums, k) {
     let zeroCount = 0;
     let max = 0;
 
-    while(end < nums.length) {
+    while(end < nums.length) { 
         if(nums[end] === 0) {
             zeroCount++;
         }
 
         while(zeroCount > k) {
-            if(nums[start] === 0) {
+            if(nums[start++] === 0) {
                 zeroCount--;
             }
-
-            start++;
         }
 
-        max = Math.max(max, end - start + 1);
+        const distance = end - start + 1;
+
+        max = Math.max(max, distance);
         end++;
     }
 
