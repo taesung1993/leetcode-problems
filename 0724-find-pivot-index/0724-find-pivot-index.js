c68c47d7b55f66105545b3f2f95fb2d761ce1929
Taesung Yun <cheonyulin@gmail.com>
2023-10-26 11:25:47 +0900

    Time: 59 ms (80.54%), Space: 43.4 MB (99.42%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 const pivotIndex = function(nums) {
-    let totalSum = nums.reduce((r, c) => r + c, 0);
     let leftSum = 0;
+    let totalSum = nums.reduce((r,c) => r + c);
 
     for(let i=0; i<nums.length; i++) {
         totalSum -= nums[i];
@@ -13,7 +13,7 @@ const pivotIndex = function(nums) {
             return i;
         }
 
-        leftSum += nums[i];
+        leftSum += nums[i]; 
     }
 
     return -1;
