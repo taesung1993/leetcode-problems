8c9f8b7d50773e4d91104565704355fd9460dda1
Taesung Yun <cheonyulin@gmail.com>
2023-10-26 12:12:27 +0900

    Time: 80 ms (70.97%), Space: 48.4 MB (64.93%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -4,30 +4,13 @@
  * @return {number[][]}
  */
 const findDifference = function(nums1, nums2) {
-    const set1 = new Set();
-    const set2 = new Set();
-    const hash1 = nums1.reduce((r, c) => {
-        r.set(c, true);
-        return r;
-    }, new Map());
-    const hash2 = nums2.reduce((r, c) => {
-        r.set(c, true);
-        return r;
-    }, new Map());
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
 
-    for(let i=0; i<nums1.length; i++) {
-        const num = nums1[i];
-
-        if(!hash2.has(num)) {
-            set1.add(num);
-        }
-    }
-
-    for(let i=0; i<nums2.length; i++) {
-        const num = nums2[i];
-
-        if(!hash1.has(num)) {
-            set2.add(num);
+    for(const item of set1) {
+        if(set2.has(item)) {
+            set1.delete(item);
+            set2.delete(item);
         }
     }
 
