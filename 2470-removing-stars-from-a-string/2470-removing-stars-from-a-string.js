0c261f201dc215c6e2682d12a690303f761c6549
Taesung Yun <cheonyulin@gmail.com>
2023-10-30 00:31:20 +0900

    Time: 73 ms (93.89%), Space: 56.9 MB (62.22%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const removeStars = function(s) {
+    const stack = [];
+
+    for(let i=0; i<s.length; i++) {
+        const char = s[i];
+
+        if(char === '*') {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
