331250f75e34dab3a4acab89d0c39f688052654e
Taesung Yun <cheonyulin@gmail.com>
2023-10-31 09:27:04 +0900

    Time: 170 ms (40.86%), Space: 52.7 MB (29.52%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
@@ -4,26 +4,21 @@
  * @return {boolean}
  */
 const closeStrings = function(word1, word2) {
-    const hash1 = new Map();
-    const hash2 = new Map();
+    const hash1 = word1.split('').reduce((m, c) => {
+        m.set(c, m.get(c) + 1 || 1);
+        return m;
+    }, new Map());
+    const hash2 = word2.split('').reduce((m, c) => {
+        m.set(c, m.get(c) + 1 || 1);
+        return m;
+    }, new Map());
 
-    for(let i=0; i<word1.length; i++) {
-        const word = word1[i];
-        hash1.set(word, hash1.get(word) + 1 || 1);
-    }   
+    const keys1 = Array.from(hash1.keys()).sort((a,b) => a>b ? 1 : a<b ? -1 : 0);
+    const keys2 = Array.from(hash2.keys()).sort((a,b) => a>b ? 1 : a<b ? -1 : 0);
+    const values1 = Array.from(hash1.values()).sort((a,b) => a-b);
+    const values2 = Array.from(hash2.values()).sort((a,b) => a-b);
+    const checkOperation1Condition = JSON.stringify(keys1) === JSON.stringify(keys2);
+    const checkOperation2Condition = JSON.stringify(values1) === JSON.stringify(values2);
 
-    for(let i=0; i<word2.length; i++) {
-        const word = word2[i];
-        hash2.set(word, hash2.get(word) + 1 || 1);
-    }
-
-    const keys1 = Array.from(hash1.keys()).sort((a,b) => a > b ? 1 : a < b ? -1 : 0);
-    const keys2 = Array.from(hash2.keys()).sort((a,b) => a > b ? 1 : a < b ?  -1 : 0);
-    const value1 = Array.from(hash1.values()).sort((a,b) => a-b);
-    const value2 = Array.from(hash2.values()).sort((a,b) => a-b);
-
-    const operation1Condition = JSON.stringify(keys1) === JSON.stringify(keys2);
-    const operation2Condition = JSON.stringify(value1) === JSON.stringify(value2);
-
-    return operation1Condition && operation2Condition
+    return checkOperation1Condition && checkOperation2Condition;
 };
