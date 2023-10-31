748d5832c4b2ee1f71c54a56464824c3e9a03f75
Taesung Yun <cheonyulin@gmail.com>
2023-10-31 09:15:11 +0900

    Time: 47 ms (85.83%), Space: 41.7 MB (96.22%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
@@ -3,15 +3,15 @@
  * @return {boolean}
  */
 const uniqueOccurrences = function(arr) {
-    const hash = new Map();
+    const map = new Map();
 
     for(let i=0; i<arr.length; i++) {
         const num = arr[i];
-        hash.set(num, hash.get(num) + 1 || 1);
+        map.set(num, map.get(num) + 1 || 1);
     }
 
-    const values = Array.from(hash.values());
-    const set = new Set(values);
-    
-    return values.length === set.size;
+    const values = Array.from(map.values());
+    const answer = values.length === new Set(values).size;
+
+    return answer;
 };
