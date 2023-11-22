6d02b8b6155265ff2b47d84a13cae74f7a2e7f4e
Taesung Yun <cheonyulin@gmail.com>
2023-11-22 09:22:08 +0900

    Time: 54 ms (84.20%), Space: 45.4 MB (44.73%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,13 +11,33 @@
  * @return {number}
  */
 const maxDepth = function(root) {
-    const getDepth = (node, depth) => {
-        if(node === null) {
-            return depth;
-        }
-        
-        return Math.max(getDepth(node.left, depth + 1), getDepth(node.right, depth + 1));
+    if(!root) {
+        return 0;
     }
 
-    return getDepth(root, 0);
+    const dfs = (node, depth) => {
+        if(!node) {
+            return depth;
+        }
+
+        if(!node.left && !node.right) {
+            return depth;
+        }
+
+        let leftDepth = 0;
+        let rightDepth = 0;
+        
+        if(node.left) {
+            leftDepth = dfs(node.left, depth + 1);
+        }
+        
+        if(node.right) {
+            rightDepth = dfs(node.right, depth + 1);
+        }
+
+        return Math.max(leftDepth, rightDepth);
+    }
+
+    const answer = dfs(root, 1);
+    return answer;
 };
