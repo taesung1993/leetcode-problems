6828ef69fa6f434e03bf6d735aade26ba20cff2c
Taesung Yun <cheonyulin@gmail.com>
2023-11-29 08:41:38 +0900

    Time: 59 ms (31.95%), Space: 43.2 MB (93.17%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -11,28 +11,29 @@
  * @return {number[]}
  */
 const rightSideView = function(root) {
-    const queue = root ? [root] : [];
-    const result = [];
-
-    while(queue.length > 0) {
-        const queueSize = queue.length;
-
-        for(let i=0; i<queueSize; i++) {
-            const node = queue.shift();
-
-            if(i === queueSize - 1) {
-                result.push(node.val);
-            }
-
-            if(node.left) {
-                queue.push(node.left);
-            }
-
-            if(node.right) {
-                queue.push(node.right);
-            }
+    const dfs = (node, depth, list) => {
+        if(!node) {
+            return list;
         }
+
+        if(list[depth]) {
+            list[depth].push(node.val);
+        } else {
+            list[depth] = [node.val];
+        }
+
+        if(!node.left && !node.right) {
+            return list;
+        }
+
+        dfs(node.left, depth + 1, list);
+        dfs(node.right, depth + 1, list);
+
+        return list;
     }
 
-    return result;
+    const list = dfs(root, 0, []);
+    const output = list.length > 0 ? list.map((el) => el.slice(-1)[0]) : [];
+
+    return output;
 };
