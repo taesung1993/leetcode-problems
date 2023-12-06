0ae9826fcadf0d8f95bb410744ea35774be30176
Taesung Yun <cheonyulin@gmail.com>
2023-12-06 23:35:51 +0900

    Time: 58 ms (44.68%), Space: 44.5 MB (29.39%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.js
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -3,23 +3,18 @@
  * @return {boolean}
  */
 const canVisitAllRooms = function(rooms) {
-    const visited = new Map([[0, true]]);
-    const queue = rooms[0];
+    const dfs = (room, visited) => {
+        if(!visited.has(room)) {
+            visited.add(room);
 
-    while(queue.length) {
-        const size = queue.length;
-
-        for(let i=0; i<size; i++) {
-            const key = queue.shift();
-
-            if(visited.has(key)) {
-                continue;
+            for(const key of rooms[room]) {
+                dfs(key, visited);
             }
-
-            visited.set(key, true);
-            queue.push(...rooms[key]);
         }
-    }
 
-    return rooms.length === visited.size;
+        return visited;
+    };
+
+    const visited = dfs(0, new Set());
+    return visited.size === rooms.length;
 };
