8edadc66ee537e38800267a4a0ddf0f048d67816
Taesung Yun <cheonyulin@gmail.com>
2023-12-16 18:15:13 +0900

    Time: 79 ms (22.09%), Space: 47.1 MB (18.74%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.js
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.js
@@ -3,61 +3,49 @@
  * @return {number}
  */
 const orangesRotting = function(grid) {
-  const queue = [];
+    let freshOrangeCount = 0;
+    const queue = grid.reduce((r, row, ridx) => {
+        r.push(...row.reduce((r, cell, cidx) => {
+            if(cell === 1) {
+                freshOrangeCount += 1;
+            }
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      const value = grid[i][j];
-
-      if (value === 2) {
-        queue.push([i, j, 0]);
-      }
-    }
-  }
-
-  const directions = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0],
-  ];
-  const visited = new Set();
-  let minute = 0;
-
-  while (queue.length > 0) {
-    const [rottenRow, rottenCol, step] = queue.shift();
-
-    for (const [dr, dc] of directions) {
-      const nextRow = dr + rottenRow;
-      const nextCol = dc + rottenCol;
-      const visitedKey = JSON.stringify([nextRow, nextCol]);
-
-      if (
-        nextRow >= 0 &&
-        nextRow <= grid.length - 1 &&
-        nextCol >= 0 &&
-        nextCol <= grid[0].length - 1 &&
-        !visited.has(visitedKey) &&
-        grid[nextRow][nextCol] === 1
-      ) {
-        visited.add(visitedKey);
-        grid[nextRow][nextCol] = 2;
-        queue.push([nextRow, nextCol, step + 1]);
-      }
+            if(cell === 2) {
+                r.push([ridx, cidx, 0]);
+            }
+            
+            return r;
+        }, []));
+        
+        return r;
+    }, []);
+    const limit = {
+        row: grid.length,
+        col: grid[0].length
     }
 
-    minute = step;
-  }
+    const directions = [[0,1], [0,-1], [1,0], [-1,0]];
+    const visited = new Set();
+    let answer = 0;
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      const value = grid[i][j];
+    while(queue.length > 0) {
+        const [currentRow, currentCol, currentMinute] = queue.shift();
 
-      if (value === 1) {
-        return -1;
-      }
+        for(const [dr, dc] of directions) {
+            const nextRow = currentRow + dr;
+            const nextCol = currentCol + dc;
+            const key = JSON.stringify([nextRow, nextCol]);
+
+            if(nextRow >= 0 && nextRow < limit.row && nextCol >=0 && nextCol < limit.col && !visited.has(key) && grid[nextRow][nextCol] === 1) {
+                grid[nextRow][nextCol] = 2;
+                freshOrangeCount -= 1;
+                queue.push([nextRow, nextCol, currentMinute + 1]);
+                visited.add(key);
+            }
+        }
+
+        answer = currentMinute;
     }
-  }
 
-  return minute;
+    return freshOrangeCount > 0 ? -1 : answer;
 };
